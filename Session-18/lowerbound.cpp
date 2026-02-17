3bc54a0e09f893f04aa46cf6230ba97ae4ce199a
Raviraj00001 <ravirajsingh123567@gmail.com>
2026-02-17 21:57:52 +0530

    Session-18
    Daa homework

---
+++ Session-18/lowerbound.cpp
@@ -0,0 +1,32 @@
+// find the lower bound of the target
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>nums={1,1,1,5,6};
+    sort(nums.begin(),nums.end());
+    int start=0,end=nums.size()-1,mid,ans=nums.size();
+    int target=0;
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        if(nums[mid]==target)
+        {
+            ans=mid;
+            end=mid-1;
+        }
+        else if(nums[mid]>target)
+        {
+            ans=mid;
+            end=mid-1;
+        }
+        else if(nums[mid]<target)
+        {
+            start=mid+1;
+        }
+    }
+    cout<<"the lower bound is = "<<ans;
+    return 0;
+}
+
